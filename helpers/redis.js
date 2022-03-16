a0af05783a5a77951bec330ecfa19e3adda6dc1e
coinlordd <35095979+coinlordd@users.noreply.github.com>
2022-03-16 15:49:53 +0100

    Update redis.js

--- helpers/redis.js
+++ helpers/redis.js
@@ -1,21 +1,22 @@
 const redis = require("redis");
 const asyncRedis = require("async-redis");
-const url = require("url");
 
-const REDIS_URL = process.env.REDIS_URL;
-const redis_uri = url.parse(REDIS_URL);
-
-const redisOptions = REDIS_URL.includes("rediss://")
-  ? {
-      port: Number(redis_uri.port),
-      host: redis_uri.hostname,
-      password: redis_uri.auth.split(":")[1],
-      db: 0,
-      tls: {
-        rejectUnauthorized: false,
-      },
-    }
-  : REDIS_URL;
+const redisOptions =
+  process.env.NODE_ENV === "production"
+    ? {
+        url: process.env.REDIS_URL,
+        socket: {
+          tls: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        url: process.env.REDIS_URL,
+        socket: {
+          tls: false,
+          rejectUnauthorized: false,
+        },
+      };
 
 const client = redis.createClient(redisOptions);
 
